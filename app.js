0dbb4ce6358da4f4321667219a430140b3f014ad
Soumya Srivastav <soumyasrivastav53@gmail.com>
2025-04-19 17:54:46 +0530

    conn. and map initialization

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 const http = require("http");
 
 const socketio = require("socket.io");
@@ -7,10 +8,14 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function(req, res) {
-    res.send("hi");
+io.on("connection", function(socket) {
+    console.log("connected");
 });
 
-app.listen(3000);
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+server.listen(3000);
