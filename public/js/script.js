ace6b0f45bf7ead29fdb58292d17e82b3986325b
Soumya Srivastav <soumyasrivastav53@gmail.com>
2025-04-19 19:17:57 +0530

    marker

--- public/js/script.js
+++ public/js/script.js
@@ -16,8 +16,28 @@ if(navigator.geolocation){
     );
 }
 
-const map = L.map("map").setView([0, 0],10);
+const map = L.map("map").setView([0, 0],16);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "OpenStreetMap"
 }).addTo(map)
+
+const markers = {};
+
+socket.on("recieve-location", (data) => {
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude]);
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    }
+    else{
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+})
+
+socket.on("user-disconnected", (id) => {
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
